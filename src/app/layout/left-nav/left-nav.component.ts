541d837055b12bbd06c4d85725437ca436cd1c99
unknown <chouaibfadli077@gmail.com>
2023-05-20 20:03:32 +0100

    the whole project commit

---
+++ src/app/layout/left-nav/left-nav.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-left-nav',
+  templateUrl: './left-nav.component.html',
+  styleUrls: ['./left-nav.component.scss'],
+})
+export class LeftNavComponent {
+  account: boolean = true;
+
+  constructor() {}
+
+  menuElms = [
+    { name: 'Home', icon: 'home', routerLink: '/home' },
+    { name: 'Messages', icon: 'message', routerLink: '/messages' },
+    { name: 'Profile', icon: 'account_box', routerLink: '/profile' },
+    { name: 'Saved Post', icon: 'save', routerLink: '/saved' },
+    { name: 'Settings', icon: 'settings', routerLink: '/settings' },
+  ];
+
+  onToggleAccount(): void {
+    this.account = !this.account;
+  }
+}
